700c7d587e38607714fda48a07de4c22015b2b17
Grant <grant.pritchard@davies-group.com>
2023-09-13 16:55:23 +0100

    a few minor style changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,30 +12,32 @@ function Navbar({ dogs }) {
 
   return (
     <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-      <Link className='navbar-brand' to='/dogs'>
-        Dog App
-      </Link>
+      <div class='container-fluid'>
+        <Link className='navbar-brand' to='/dogs'>
+          Dog App
+        </Link>
 
-      <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarNav'
-        aria-controls='navbarNav'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
-      >
-        <span className='navbar-toggler-icon' />
-      </button>
-      <div className='collapse navbar-collapse' id='navbarNav'>
-        <ul className='navbar-nav'>
-          <li className='nav-item'>
-            <NavLink to='/dogs' end className='nav-link'>
-              Home
-            </NavLink>
-          </li>
-          {dogLinks}
-        </ul>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon' />
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <NavLink to='/dogs' end className='nav-link'>
+                Home
+              </NavLink>
+            </li>
+            {dogLinks}
+          </ul>
+        </div>
       </div>
     </nav>
   );
